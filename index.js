f76805edff9da910b6febb3a81e3ea9b8985101d
Ivan Echevarria <iechevarria@email.wm.edu>
2018-01-08 12:16:59 -0500

    Add move, moveLeft logic

--- index.js
+++ index.js
@@ -34,18 +34,46 @@ var State = {
 
 
 /* Logic */
+
+function move (direction) {
+  if (direction === 'l') { moveLeft(); }
+  else if (direction === 'r') { moveRight(); }
+  else if (direction === 'u') { moveUp(); }
+  else if (direction === 'd') { moveDown();}
+  resetCombined();
+}
+
 function moveRight () {
-  for (var pass = 0; pass < State.width - 2; pass++) {
+  for (var pass = 0; pass < State.width - 1; pass++) {
     for (var i = 0; i < State.height; i++) {
       for (var j = State.width - 1; j > 0; j--) {
         cur = State.getSquare(i, j);
-        left = State.getSquare(i, j-1);
+        left = State.getSquare(i, j - 1);
         if (cur.value === 0) {
           cur.value = left.value;
           left.value = 0;
         } else if (cur.value === left.value && !cur.combined && !left.combined) {
           cur.value *= 2;
           left.value = 0;
+          cur.combined = true;
+        }
+      }
+    }
+  }
+}
+
+function moveLeft () {
+  for (var pass = 0; pass < State.width - 1; pass++) {
+    for (var i = 0; i < State.height; i++) {
+      for (var j = 0; j < State.width - 1; j++) {
+        cur = State.getSquare(i, j);
+        right = State.getSquare(i, j + 1);
+        if (cur.value === 0) {
+          cur.value = right.value;
+          right.value = 0;
+        } else if (cur.value === right.value && !cur.combined && !right.combined) {
+          cur.value *= 2;
+          right.value = 0;
           cur.combined = true;
         }
       }
@@ -70,5 +98,7 @@ State.getSquare(0, 1).value = 2;
 State.getSquare(0, 2).value = 2;
 State.getSquare(0, 3).value = 2;
 State.print();
-moveRight();
+move('l');
+State.print();
+move('r');
 State.print();
