2c3070d130f15f5e2ec90e23468e222758ccb501
Ivan Echevarria <iechevarria@email.wm.edu>
2018-01-29 22:13:18 -0500

    Add index.html, refactor index,js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ function Square () {
   this.combined = false;
 }
 
-var State = {
-  height: 4,
-  width: 4,
-  score: 0,
 
-  board: new Array(this.height),
+var State = {
+  size: 4,
+  score: 0,
+  board: new Array(this.size),
+
   getSquare: function (i, j) {
     return this.board[i][j];
   },
   
   reset: function () {
-    for (var i = 0; i < this.height; i++) {
-      this.board[i] = new Array(this.width);
-      for (var j = 0; j < this.width; j++) {
+    for (var i = 0; i < this.size; i++) {
+      this.board[i] = new Array(this.size);
+      for (var j = 0; j < this.size; j++) {
         this.board[i][j] = new Square();
       }
     }
@@ -26,9 +26,9 @@ var State = {
   
   print: function () {
     console.log('score: ' + this.score); 
-    for (var i = 0; i < this.height; i++) {
+    for (var i = 0; i < this.size; i++) {
       var myStr = '';      
-      for (var j = 0; j < this.width; j++) {
+      for (var j = 0; j < this.size; j++) {
         if (this.board[i][j].value != 0) { myStr += this.board[i][j].value + ' '; }
         else { myStr += '. '; }
       }
@@ -39,88 +39,80 @@ var State = {
 };
 
 
-/* Logic */
-
-function move (direction) {
-  if (direction === 'l' || direction === 'r') {
-    for (var pass = 0; pass < State.width - 1; pass++) {
-      for (var i = 0; i < State.height; i++) {
+var Logic = {
+  move: function (direction) {
+    for (var pass = 0; pass < State.size - 1; pass++) {
+      for (var n = 0; n < State.size; n++) {
         if (direction === 'l') {
-          for (var j = 0; j < State.width - 1; j++) {
-            mv(i, j, 0, 1);
+          for (var j = 0; j < State.size - 1; j++) {
+            this.combine(n, j, 0, 1);
+          }
+        } else if (direction === 'r') {
+          for (var j = State.size - 1; j > 0; j--) {
+            this.combine(n, j, 0, -1);
+          }
+        } else if (direction === 'u') {
+          for (var i = 0; i < State.size - 1; i++) {
+            this.combine(i, n, 1, 0);
           }
         } else {
-          for (var j = State.width - 1; j > 0; j--) {
-            mv(i, j, 0, -1);
+          for (var i = State.size - 1; i > 0; i--) {
+            this.combine(i, n, -1, 0);
           }
         }
       }
     }
-  } 
-  else if (direction === 'u' || direction === 'd') {
-    for (var pass = 0; pass < State.height - 1; pass++) {
-      for (var j = 0; j < State.width; j++) {
-        if (direction === 'u') {
-          for (var i = 0; i < State.height - 1; i++) {
-            mv(i, j, 1, 0);
-          }
-        } else {
-          for (var i = State.height - 1; i > 0; i--) {
-            mv(i, j, -1, 0);
-          }
+    this.resetCombined();
+    this.addSquare();
+    State.print();  
+    this.validMoveExists();
+  },
+
+  combine: function (i, j, di, dj) {
+    cur = State.getSquare(i, j);
+    adj = State.getSquare(i + di, j + dj);
+    if (cur.value === 0) {
+      cur.value = adj.value;
+      adj.value = 0;
+    } else if (cur.value === adj.value && !cur.combined && !adj.combined) {
+      cur.value *= 2;
+      State.score += cur.value;          
+      adj.value = 0;
+      cur.combined = true;
+    }
+  },
+
+  resetCombined: function () {
+    for (var i = 0; i < State.size; i++) {
+      for (var j = 0; j < State.size; j++) {
+        State.getSquare(i, j).combined = false;
+      }
+    }
+  },
+
+  validMoveExists: function () {
+    var moveExists = false;
+    for (var i = 0; i < State.size - 1; i++) {
+      for (var j = 0; j < State.size - 1; j++) {
+        if (State.getSquare(i, j).value === 0 || State.getSquare(i + 1, j).value === 0 || State.getSquare(i, j + 1).value === 0 ||
+            State.getSquare(i, j).value === State.getSquare(i + 1, j).value ||
+            State.getSquare(i, j).value === State.getSquare(i, j + 1).value) 
+        {
+          moveExists = true;
         }
       }
     }
-  }
-  resetCombined();
-  addSquare();
-  validMoveExists();
-}
-
-function mv (i, j, di, dj) {
-  cur = State.getSquare(i, j);
-  adj = State.getSquare(i + di, j + dj);
-  if (cur.value === 0) {
-    cur.value = adj.value;
-    adj.value = 0;
-  } else if (cur.value === adj.value && !cur.combined && !adj.combined) {
-    cur.value *= 2;
-    State.score += cur.value;          
-    adj.value = 0;
-    cur.combined = true;
-  }
-}
-
-function resetCombined () {
-  for (var i = 0; i < State.height; i++) {
-    for (var j = 0; j < State.width; j++) {
-      State.getSquare(i, j).combined = false;
+    if (State.getSquare(State.size - 1, State.size - 1).value === 0 ||
+        State.getSquare(State.size - 1, State.size - 1).value === State.getSquare(State.size - 2, State.size - 1).value ||
+        State.getSquare(State.size - 1, State.size - 1).value === State.getSquare(State.size - 1, State.size - 2).value)
+    {
+      moveExists = true;
     }
-  }
-}
+    return moveExists;
+  },
 
-function validMoveExists () {
-  var moveExists = false;
-  for (var i = 0; i < State.height - 1; i++) {
-    for (var j = 0; j < State.width - 1; j++) {
-      if (State.getSquare(i, j).value === 0 || State.getSquare(i + 1, j).value === 0 || State.getSquare(i, j + 1).value === 0 ||
-          State.getSquare(i, j).value === State.getSquare(i + 1, j).value ||
-          State.getSquare(i, j).value === State.getSquare(i, j + 1).value) 
-      {
-        moveExists = true;
-      }
-    }
-  }
-  if (State.getSquare(State.height - 1, State.width - 1).value === 0 ||
-      State.getSquare(State.height - 1, State.width - 1).value === State.getSquare(State.height - 2, State.width - 1).value ||
-      State.getSquare(State.height - 1, State.width - 1).value === State.getSquare(State.height - 1, State.width - 2).value)
-  {
-    moveExists = true;
-  }
-  return moveExists;
-}
-
-function addSquare () {}
+  addSquare: function () {}
+};
 
 /* View */
 
@@ -143,16 +135,16 @@ window.onkeydown = function (e) {
 window.onkeyup = function (e) {
   var key = e.keyCode ? e.keyCode : e.which;
   if (key == 37 && leftPressed) {
-    move('l');
+    Logic.move('l');
     leftPressed = false;
   } else if (key === 38 && upPressed) {
-    move('u');
+    Logic.move('u');
     upPressed = false;
   } else if (key === 39 && rightPressed) {
-    move('r');
+    Logic.move('r');
     rightPressed = false;
   } else if (key === 40 && downPressed) {
-    move('d');
+    Logic.move('d');
     downPressed = false;
   }
 };
@@ -165,13 +157,3 @@ State.getSquare(0, 2).value = 2;
 State.getSquare(0, 3).value = 2;
 State.getSquare(1, 3).value = 2;
 State.getSquare(3, 3).value = 2;
-State.print();
-move('d');
-State.print();
-move('r');
-State.print();
-move('l');
-State.print();
-move('u');
-State.print();
-console.log(validMoveExists());
